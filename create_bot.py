dc36c013a5c711d6332d4a745b9ca3436c32cf3a
Bozhena <baltinabozhe@gmail.com>
2026-05-28 00:52:43 +0300

    Убрала APScheduler

--- create_bot.py
+++ create_bot.py
@@ -7,7 +7,6 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from decouple import Config, RepositoryEnv
 
 import os
